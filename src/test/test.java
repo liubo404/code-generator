1520107352b13f3ece7ff5d5555b534a94fb3448
verphen <verphen@163.com>
2014-06-18 16:02:00 +0800

    update test

--- src/test/test.java
+++ src/test/test.java
@@ -1,19 +1,5 @@
 package test;
 
-import main.java.com.verphen.db.db;
-import main.java.com.verphen.generate.generateThing;
-
 public class test {
-	
-	
-	public static void main(String[] args) throws Exception{
-		db.initBruce();
-//		ConnectServer.init();
-//     	generateThing.generateAll("yunlu","D://opt//dir33");
-		generateThing.generateOneModle("yl_user", "D://opt//dir88");
-	}
-	
-	
+
 }
-
-
