14a3a688f6e615d40d12d63f3c9b078f8124c95a
verphen <verphen@163.com>
2014-12-22 23:02:25 +0800

    update readme.mmd

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-CodeBuilder
-===================
 
-	Code auto build for java
+web项目开发步骤：
+
+- 客户需求分析
+
+- 设计数据库文档，创建物理数据库
+
+- Coding，设计MVC各层包结构
+
+为提高生产率，此项目即为自动生成MVC各层 基本代码
--- src/main/java/com/effine/model/Table.java
+++ src/main/java/com/effine/model/Table.java
@@ -9,36 +9,37 @@ public class Table {
 	private String packageName;
 	public static String domain = "yunlu";
 	private List<Column> propertyList;
-	
-	public Table(){
-		
-	}
-	
+
 	public String getModelName() {
 		return modelName;
 	}
+
 	public void setModelName(String modelName) {
 		this.modelName = modelName;
 	}
+
 	public String getTableName() {
 		return tableName;
 	}
+
 	public void setTableName(String tableName) {
 		this.tableName = tableName;
 	}
+
 	public List<Column> getPropertyList() {
 		return propertyList;
 	}
+
 	public void setPropertyList(List<Column> propertyList) {
 		this.propertyList = propertyList;
 	}
+
 	public String getPackageName() {
 		return packageName;
 	}
+
 	public void setPackageName(String packageName) {
 		this.packageName = packageName;
 	}
-	
-	
-	
+
 }
